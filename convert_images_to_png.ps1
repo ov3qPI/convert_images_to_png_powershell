e2e3fff8d6e71dcd0a742ee4c911d33f3213689b
ov3qPI <147228776+ov3qPI@users.noreply.github.com>
2025-02-21 17:25:52 -0700

    Update convert_images_to_png.ps1

--- convert_images_to_png.ps1
+++ convert_images_to_png.ps1
@@ -1,23 +1,18 @@
-# Initialize counters
 $count = 0
 $total = 0
 
-# Count how many .jpg and .jpeg files are in the directory and subdirectories
-Get-ChildItem -Path D:\ -Recurse -Include *.jpg, *.jpeg *.webp | ForEach-Object {
-    $total++
-}
+$files = Get-ChildItem -Path E:\ -Recurse -Include *.jpg, *.jpeg, *.webp
+$total = $files.Count
 
-# Process the files
-Get-ChildItem -Path D:\ -Recurse -Include *.jpg, *.jpeg *.webp | ForEach-Object {
-    $file = $_
+foreach ($file in $files) {
     $outputFile = "$($file.DirectoryName)\$($file.BaseName).png"
     
-    # Try to convert the file and move it if successful
     try {
         magick $file.FullName -quality 100 $outputFile
+        
         if ($?) {
-            Move-Item -Path $outputFile -Destination D:\Pictures\png\
             Remove-Item -Path $file.FullName
+            Rename-Item -Path $outputFile -NewName $file.Name
             $count++
             Write-Host "Converted $count out of $total files."
         } else {
